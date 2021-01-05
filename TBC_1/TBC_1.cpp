2477d40020c8add711f6389812c2b37082833715
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-01-05 13:02:30 +0900

    2
    2

--- TBC_1/TBC_1.cpp
+++ TBC_1/TBC_1.cpp
@@ -2,6 +2,35 @@
 #include <iostream>
 using namespace std;
 
+//int calculator(int a, int b, char c); // 리턴값이 없다면 void로 선언
+//
+//int calculator(int a, int b, char c) // 계산기함수 선언 
+//{
+//	int calc;
+//
+//	if (c == '+')
+//	{
+//		calc = a + b;
+//	}
+//		
+//	else if (c == '-')
+//	{
+//		calc = a - b;
+//	}
+//
+//	else if (c == '*')
+//	{
+//		calc = a * b;
+//	}
+//
+//	else if (c == '/')
+//	{
+//		calc = a / b;
+//	}
+//
+//	return calc;
+//}
+
 int main()
 {
 	//소스코드 - 컴파일 -> 오브젝트파일
@@ -17,6 +46,39 @@ int main()
 
 	//변수 : 주소의 다른 이름이라고 생각
 
-	int x0 = 1;
-	cout << &x0 << endl; // 변수 x0의 주소를 출력
+	//int x0 = 1;
+	//cout << &x0 << endl; // 변수 x0의 주소를 출력
+
+	//cout << calculator(10, 2, '+') << endl;
+	//cout << calculator(10, 2, '-') << endl;
+	//cout << calculator(10, 2, '*') << endl;
+	//cout << calculator(10, 2, '/') << endl;
+
+	//변수를 선언할때 예약어는 사용하면 안됨, 숫자로 시작하면 안됨
+	//int n_banana;
+	//int nBanana;
+	//int _banana;
+	//변수를 선언할때는 하나의 규칙을 유지하는것이 좋다
+	//그러나 결국 변수 선언 방식은 내가 들어가는 그룹에 맞춰야한다. 
+
+	/* local scope*/
+	
+	//{} 중괄호 안에 선언된 변수는 중괄호 밖을 벗어나서는 사용할수없다
+	
+	//지역변수는 영역을 벗어나면 사용할수없음
+	//지역변수가 차지하고 있던 메모리는 지역변수가 영역 밖으로 벗어날때 
+	//스택메모리에 반납됨. 반납된 메모리는 다음 지역변수가 사용할수있도록 대기
+
+	//int x = 2; // x is a variable, 2 is a literal
+
+	//int y = (x > 0) ? 1 : 2; // 삼항연산자 : 괄호안의 값이 참이면 1, 거짓이면 2
+	
+	// 이렇게 하드코딩을 할때 줄맞춤을 함으로써 한눈에 보기 좋음
+	//int x					= 10;
+	//int a_values			= 5;
+	//int banana_price		= 10;
+	//char first_alphabet	= 'a';
+
+
 }
+
